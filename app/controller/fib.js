2c3afc2b3900a7dcd802fa2d7e80676c0341486f
Matheus Mariano <vmatheus.mariano@gmail.com>
2018-01-29 14:27:15 -0300

    Change promises to async/await pattern

--- app/controller/fib.js
+++ app/controller/fib.js
@@ -1,47 +1,52 @@
-const amqp = require('../services/amqp');
 const uuid = require('uuid/v1');
+const co = require('co');
+const amqp = require('../services/amqp');
 
 module.exports = {
   calculate: (request, response) => {
-    amqp.connect().then(connection => {
-      connection.createChannel().then(channel => {
-        channel.assertQueue('', {
-          exclusive: true,
-        }).then(queue => {
-          const correlationId = uuid();
+    const wrapper = co.wrap(function * () {
+      const connection = yield amqp.connect();
 
-          const serverQueue = 'rpc_queue';
+      const channel = yield connection.createChannel()
 
-          const queueName = queue.queue;
+      const queue = yield channel.assertQueue('', {
+        exclusive: true,
+      })
 
-          const number = request.body.number;
+      const correlationId = uuid();
 
-          console.log(` [x] Requesting fib(${number}).`);
+      const serverQueue = 'rpc_queue';
 
-          channel.consume(queueName, message => {
-            if (message.properties.correlationId === correlationId) {
-              const result = message.content.toString();
+      const queueName = queue.queue;
 
-              console.log(` [.] Got ${result}.`);
+      const number = request.body.number;
 
-              connection.close();
+      console.log(` [x] Requesting fib(${number}).`);
 
-              response.json({
-                data: {
-                  result
-                }
-              });
+      channel.consume(queueName, message => {
+        if (message.properties.correlationId === correlationId) {
+          const result = message.content.toString();
+
+          console.log(` [.] Got ${result}.`);
+
+          connection.close();
+
+          response.json({
+            data: {
+              result
             }
           });
-
-          channel.sendToQueue(serverQueue, new Buffer(number.toString()), {
-            correlationId,
-            replyTo: queueName,
-          });
-        }, {
-          noAck: true,
-        });
+        }
       });
+
+      channel.sendToQueue(serverQueue, new Buffer(number.toString()), {
+        correlationId,
+        replyTo: queueName,
+      });
+    }, {
+      noAck: true,
     });
+
+    wrapper();
   },
 };
